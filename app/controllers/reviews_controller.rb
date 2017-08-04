f900c5f957753edc99545ddd5ecb2bba94e5184d
egomatsushita <eduardo.go.rj@gmail.com>
2017-08-04 19:49:10 +0000

    Add create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def create
+    raise "Yay, I'm here!"
+
+    @product_id = params[:product_id]
+    review = Review.new(review_params)
+    review.user_id = current_user.id
+    review.product_id = @product_id
+
+    if review.save
+      redirect_to product_path(id: @product_id), notice: 'Review created'
+    else
+      redirect_to product_path(id: @product_id)
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
 end
