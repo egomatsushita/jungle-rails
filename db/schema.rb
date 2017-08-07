88bf7aad6296508752f7ed37af309be9b1fb308b
egomatsushita <eduardo.go.rj@gmail.com>
2017-08-07 15:40:00 +0000

    Auto-generated model specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804161216) do
+ActiveRecord::Schema.define(version: 20170804145543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,4 @@ ActiveRecord::Schema.define(version: 20170804161216) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
-  add_foreign_key "reviews", "products"
-  add_foreign_key "reviews", "users"
 end
